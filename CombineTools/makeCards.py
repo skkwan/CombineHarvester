76c90af553d5f5d6e19887934e692d75db9270c0
Stephanie Kwan <skwan@princeton.edu>
2024-04-12 13:33:48 +0200

    write all files to cards/

--- CombineTools/makeCards.py
+++ CombineTools/makeCards.py
@@ -1,18 +1,27 @@
 import os 
 
+os.system("ulimit -s unlimited")
 
-command="haabbtt mutau 2018 4 lowMassSR mediumMassSR highMassSR highMassCR"
+command="haabbtt mutau 2018 3 lowMassSR mediumMassSR highMassSR"
 
 os.system(command)
 
+
+os.system("mv haabbtt*.txt cards/")
+
 masspoints = [15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
-# masspoints = [15]
-
+# masspoints = [60]
 for masspoint in masspoints:
-    os.system('echo "* autoMCStats 0.0" >> haabbtt_mutau_1_2018_{}.txt'.format(masspoint))
-    os.system('echo "* autoMCStats 0.0" >> haabbtt_mutau_2_2018_{}.txt'.format(masspoint))
-    os.system('echo "* autoMCStats 0.0" >> haabbtt_mutau_3_2018_{}.txt'.format(masspoint))
+    os.system('echo "* autoMCStats 0.0" >> cards/haabbtt_mutau_1_2018_{}.txt'.format(masspoint))
+    os.system('echo "* autoMCStats 0.0" >> cards/haabbtt_mutau_2_2018_{}.txt'.format(masspoint))
+    os.system('echo "* autoMCStats 0.0" >> cards/haabbtt_mutau_3_2018_{}.txt'.format(masspoint))
 
-    os.system('combineCards.py haabbtt_mutau_1_2018_{}.txt haabbtt_mutau_2_2018_{}.txt haabbtt_mutau_3_2018_{}.txt > combined_mutau_2018_{}.txt'.format(masspoint, masspoint, masspoint, masspoint))
+    os.system('combineCards.py cards/haabbtt_mutau_1_2018_{}.txt cards/haabbtt_mutau_2_2018_{}.txt cards/haabbtt_mutau_3_2018_{}.txt > cards/combined_mutau_2018_{}.txt'.format(masspoint, masspoint, masspoint, masspoint))
+    
+    os.system("mv *.root cards/")
+    os.system('combine -M AsymptoticLimits cards/combined_mutau_2018_{}.txt -t -1 -m {} | tee cards/limits_{}'.format(masspoint, masspoint, masspoint))
 
-    os.system('combine -M AsymptoticLimits combined_mutau_2018_{}.txt -t -1 -m {} | tee limits_{}'.format(masspoint, masspoint, masspoint))
+os.system("mv higgsCombineTest.AsymptoticLimits*.root cards/")
+os.system('hadd -f -j -k cards/higgsCombine_aa_bbtt.root cards/higgsCombineTest.AsymptoticLimits.mH*.root')
+
+print(">>> makeCards.py: Next step: copy cards/higgsCombine_aa_bbtt.root to LUNA limits/ folder: /afs/cern.ch/work/s/skkwan/public/hToA1A2/CMSSW_13_2_6_patch2/src/lunaFramework/limits/")
