bad0398b5c3f545e21b25a62cf394d854b43564f
Stephanie Kwan <skwan@princeton.edu>
2024-04-25 15:33:39 +0200

    do impacts with expectSignal 0 and 1

--- CombineTools/doImpacts.py
+++ CombineTools/doImpacts.py
@@ -6,12 +6,13 @@ import os
 
 # os.system('text2workspace.py "asymmCards/combined_mutau_2018_60_20.txt" -m 60')
 
-os.system('combineTool.py -M Impacts -d asymmCards/combined_mutau_2018_60_20.root -m 60 --doInitialFit --robustFit 1 --expectSignal=0 --rMin=-40 --rMax=40')
+for value in [0, 1]:
+    os.system(f'combineTool.py -M Impacts -d asymmCards/combined_mutau_2018_60_20.root -m 60 --doInitialFit --robustFit 1 --expectSignal {value} --rMin -40 --rMax 40')
 
-os.system('combineTool.py -M Impacts -d asymmCards/combined_mutau_2018_60_20.root -m 60 --robustFit 1 --doFits --parallel=150 --expectSignal=0 --rMin=-40 --rMax=40')
+    os.system(f'combineTool.py -M Impacts -d asymmCards/combined_mutau_2018_60_20.root -m 60 --robustFit 1 --doFits --parallel 150 --expectSignal {value} --rMin -40 --rMax 40')
 
-os.system('combineTool.py -M Impacts -d asymmCards/combined_mutau_2018_60_20.root -m 60 -o impacts.json')
+    os.system(f'combineTool.py -M Impacts -d asymmCards/combined_mutau_2018_60_20.root -m 60 -o impacts_expectSignal{value}.json')
 
-os.system('plotImpacts.py -i impacts.json -o impacts_60_20')
+    os.system(f'plotImpacts.py -i impacts_expectSignal{value}.json -o impacts_60_20_expectSignal{value}')
 
-os.system('cp impacts_60_20.pdf /eos/user/s/skkwan/www/limits/')
+    os.system(f'cp impacts_60_20_expectSignal{value}.pdf /eos/user/s/skkwan/www/limits/')
