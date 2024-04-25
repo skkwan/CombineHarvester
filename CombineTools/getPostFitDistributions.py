1cb616458f12355853610f8b447b5f84bd6c4204
Stephanie Kwan <skwan@princeton.edu>
2024-04-25 17:50:29 +0200

    give datacard to PostFitShapesFromWorkspace so it rebins correctly

--- CombineTools/getPostFitDistributions.py
+++ CombineTools/getPostFitDistributions.py
@@ -6,5 +6,6 @@ import os
 os.system('combine -M FitDiagnostics asymmCards/combined_mutau_2018_60_20.root -m 600 --saveShapes --saveWithUncertainties')
 
 # creates fitDiagnosticsTest.root 
-
-os.system(f'PostFitShapesFromWorkspace --workspace asymmCards/combined_mutau_2018_60_20.root --fitresult fitDiagnosticsTest.root:fit_s --postfit --output asymmCards/postfitShapes_a1a2_60_20.root')
+os.system(f'cp asymmCards/combined_mutau_2018_60_20.txt .')
+os.system(f'cp asymmCards/combined_mutau_2018_60_20.root .')
+os.system(f'PostFitShapesFromWorkspace --datacard combined_mutau_2018_60_20.txt --workspace combined_mutau_2018_60_20.root --fitresult fitDiagnosticsTest.root:fit_s --postfit --output postfitShapes_a1a2_60_20.root')
