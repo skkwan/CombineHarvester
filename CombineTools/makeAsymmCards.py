c03e74c4e402d3841b7608ef60f49e34567b8064
skkwan <skwan@princeton.edu>
2024-12-10 11:27:18 +0100

    use user and first letter

--- CombineTools/makeAsymmCards.py
+++ CombineTools/makeAsymmCards.py
@@ -1,10 +1,11 @@
-import os 
+import os
+import re 
 
 doBlindedLimits = True
 
 os.system("ulimit -s unlimited")
 
-channel = "mutau"
+channels = ["mutau", "etau", "emu"]
 year = "2018"
 # either 4b2t (cascade) or 2b2t (non-cascade). Must match the name of the signal histograms in the datacard
 signalType = "2b2t" 
@@ -13,11 +14,11 @@ signalType = "2b2t"
 allMasses = {
     "4b2t": [   
                 # assuming lightest scalar is 15 GeV, 
-                [[30, 15], [40, 15], [50, 15], [60, 15], [70, 15], [80, 15], [90, 15], [100, 15]],
+                [[30, 15] , [40, 15], [50, 15], [60, 15], [70, 15], [80, 15], [90, 15], [100, 15]],
                 # next, assuming lightest scalar is 20 GeV
                 [[40, 20], [50, 20], [60, 20], [70, 20], [80, 20], [90, 20], [100, 20]], 
                 #   # lastly, 30 GeV
-                [[60, 30], [70, 30], [80, 30], [90, 30]]
+                [[60, 30], [70, 30], [80, 30], [90, 30]],
             ],
     "2b2t": [
                 [[20, 15], [30, 15]],
@@ -29,43 +30,60 @@ allMasses = {
 }
 
 
-for massList in allMasses[signalType]:
-    for masspoint in massList:
-        command=f"hToA1A2 {channel} {signalType} {year} {masspoint[0]} {masspoint[1]} 3 lowMassSR mediumMassSR highMassSR"
-        os.system(command)
+for channel in channels:
+    for massList in allMasses[signalType]:
+        for masspoint in massList:
+            command=f"hToA1A2 {channel} {signalType} {year} {masspoint[0]} {masspoint[1]} 3 lowMassSR mediumMassSR highMassSR"
+            os.system(command)
 
-    os.system("mv hToA1A2*.txt asymmCards/")
-    os.system("mv hAsymm*.root asymmCards/")
+        os.system("mv hToA1A2*.txt asymmCards/")
+        os.system("mv hAsymm*.root asymmCards/")
 
-    for masspoint in massList:
-        os.system(f'echo "* autoMCStats 0.0" >> asymmCards/hToA1A2_{channel}_1_{year}_{masspoint[0]}_{masspoint[1]}.txt')
-        os.system(f'echo "* autoMCStats 0.0" >> asymmCards/hToA1A2_{channel}_2_{year}_{masspoint[0]}_{masspoint[1]}.txt')
-        os.system(f'echo "* autoMCStats 0.0" >> asymmCards/hToA1A2_{channel}_3_{year}_{masspoint[0]}_{masspoint[1]}.txt')
+        for masspoint in massList:
+            ch1_filename   = f"asymmCards/hToA1A2_{channel}_1_{year}_{masspoint[0]}_{masspoint[1]}.txt"
+            ch2_filename   = f"asymmCards/hToA1A2_{channel}_2_{year}_{masspoint[0]}_{masspoint[1]}.txt"
+            ch3_filename   = f"asymmCards/hToA1A2_{channel}_3_{year}_{masspoint[0]}_{masspoint[1]}.txt"
+            allch_filename = f"asymmCards/hToA1A2_{channel}_allcats_{year}_{masspoint[0]}_{masspoint[1]}.txt"
+            os.system(f'echo "* autoMCStats 0.0" >> {ch1_filename}')
+            os.system(f'echo "* autoMCStats 0.0" >> {ch2_filename}')
+            os.system(f'echo "* autoMCStats 0.0" >> {ch3_filename}')
 
-        os.system(f'combineCards.py asymmCards/hToA1A2_{channel}_1_{year}_{masspoint[0]}_{masspoint[1]}.txt asymmCards/hToA1A2_{channel}_2_{year}_{masspoint[0]}_{masspoint[1]}.txt asymmCards/hToA1A2_{channel}_3_{year}_{masspoint[0]}_{masspoint[1]}.txt > asymmCards/combined_{channel}_{year}_{masspoint[0]}_{masspoint[1]}.txt')
+            os.system(f'combineCards.py {ch1_filename} {ch2_filename} {ch3_filename} > {allch_filename}')
 
-        # Convert text to workspace
-        os.system(f'text2workspace.py "asymmCards/combined_{channel}_{year}_{masspoint[0]}_{masspoint[1]}.txt" -m {masspoint[0]}')
+            # Convert text file to workspace
+            os.system(f'text2workspace.py "{allch_filename}" -m {masspoint[0]}')
 
+        for masspoint in massList:
+            allch_workspace_rootfile = f"asymmCards/hToA1A2_{channel}_allcats_{year}_{masspoint[0]}_{masspoint[1]}.root"
+            logfile = f"asymmCards/limits_allcats_{year}_{channel}_{masspoint[0]}_{masspoint[1]}"
+            if (doBlindedLimits):  # -t -1 option
+                combineCommand = f'combine -M AsymptoticLimits {allch_workspace_rootfile} --rMin=-10 -t -1 -m {masspoint[0]} | tee {logfile}'
+            else: 
+                combineCommand = f'combine -M AsymptoticLimits {allch_workspace_rootfile} -m {masspoint[0]} | tee {logfile}'
+            os.system(combineCommand)
 
-    for masspoint in massList:
-        if (doBlindedLimits):  # -t -1 option
-            combineCommand = f'combine -M AsymptoticLimits asymmCards/combined_{channel}_{year}_{masspoint[0]}_{masspoint[1]}.root --rMin=-10 --run blind -t -1 -m {masspoint[0]} | tee asymmCards/limits_{masspoint[0]}_{masspoint[1]}'
-        else: 
-            combineCommand = f'combine -M AsymptoticLimits asymmCards/combined_{channel}_{year}_{masspoint[0]}_{masspoint[1]}.root -m {masspoint[0]} | tee asymmCards/limits_{masspoint[0]}_{masspoint[1]}'
-        # this makes a file called higgsCombineTest.AsymptoticLimits.mH60.root
-        os.system(combineCommand)
+            # this produces a file called higgsCombineTest.AsymptoticLimits.mH30.root, higgsCombineTest.AsymptoticLimits.mH40.root in the top-level directory for (30, 15)
+            oldName = f'higgsCombineTest.AsymptoticLimits.mH{masspoint[0]}.root'
+            # rename this file to something more descriptive that will not get overwritten, and move it to asymmCards/
+            newName = f"higgsCombineTest.AsymptoticLimits.{year}_{channel}.m2_{masspoint[0]}.m1_{masspoint[1]}.root"
+            os.system(f"mv {oldName} asymmCards/{newName}")
 
-    os.system("rm asymmCards/higgsCombineTest.AsymptoticLimits*.root")
-    os.system("mv higgsCombineTest.AsymptoticLimits*.root asymmCards/")
+        # Remember we are itearting over massList, which is e.g. (40, 15), (50, 15), etc. all with the same m_1 = 15 GeV. Get this m_1, can just get the 0th entry, 1st element
+        thisM1Point = massList[0][1]
+        # Next we need to hadd the corresponding mass points but in ascending order. 
+        filesToHadd = ""
+        for masspoint in massList:
+            filesToHadd += f"asymmCards/higgsCombineTest.AsymptoticLimits.{year}_{channel}.m2_{masspoint[0]}.m1_{masspoint[1]}.root"
+            filesToHadd += " "
+        print(filesToHadd)
+        # add the signalType to the file name, to distinguish the cascade and non-cascade ones
+        os.system(f'hadd -f -j -k asymmCards/higgsCombine_a1a2_{signalType}_{year}_{channel}_m1_{thisM1Point}.root {filesToHadd}')
 
-    # Each entry in massList assumes the same m_1. Get this number. 
-    thisM1Point = massList[0][1]
-    # Next we need to hadd the corresponding mass points but in ascending order. 
-    filesToHadd = ""
-    for masspoint in massList:
-        filesToHadd += f"asymmCards/higgsCombineTest.AsymptoticLimits.mH{masspoint[0]}.root "
-    print(filesToHadd)
-    os.system(f'hadd -f -j -k asymmCards/higgsCombine_a1a2_{year}_{channel}_m1_{thisM1Point}.root {filesToHadd}')
+# Get user name and first letter
+user=os.environ["USER"]
+r = re.compile("^(.)")
+m = r.match(user)
+firstletter = m.group()
 
-    print("Next step: Make limit plots")
+suggested_command = f"cp asymmCards/higgsCombine_a1a2_{signalType}_{year}_*.root /afs/cern.ch/work/{firstletter}/{user}/public/hToA1A2/CMSSW_13_2_6_patch2/src/lunaFramework/limits/"
+print(f"Next step: Make limit plots. Copy the output files to the LUNA framework plotting directory subfolder: e.g. {suggested_command}")
