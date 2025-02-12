968115a87a0688e70d3f1a23ccb90555c7d99385
skkwan <skwan@princeton.edu>
2025-02-12 22:36:36 +0100

    add channel option to getPostFitDistributions.py

--- CombineTools/getPostFitDistributions.py
+++ CombineTools/getPostFitDistributions.py
@@ -5,13 +5,15 @@ import os
 
 masspoints = [[80, 20]]
 
+channel = "mutau"
+
 for masspair in masspoints:
     m1 = masspair[0]
     m2 = masspair[1]
 
-    os.system(f'combine -M FitDiagnostics asymmCards/combined_mutau_2018_{m1}_{m2}.root -m {m1} --saveShapes --saveWithUncertainties')
+    os.system(f'combine -M FitDiagnostics asymmCards/combined_{channel}_2018_{m1}_{m2}.root -m {m1} --saveShapes --saveWithUncertainties')
 
     # creates fitDiagnosticsTest.root 
-    os.system(f'cp asymmCards/combined_mutau_2018_{m1}_{m2}.txt .')
-    os.system(f'cp asymmCards/combined_mutau_2018_{m1}_{m2}.root .')
-    os.system(f'PostFitShapesFromWorkspace --datacard combined_mutau_2018_{m1}_{m2}.txt --workspace combined_mutau_2018_{m1}_{m2}.root --fitresult fitDiagnosticsTest.root:fit_s --postfit --output postfitShapes_a1a2_{m1}_{m2}.root')
+    os.system(f'cp asymmCards/combined_{channel}_2018_{m1}_{m2}.txt .')
+    os.system(f'cp asymmCards/combined_{channel}_2018_{m1}_{m2}.root .')
+    os.system(f'PostFitShapesFromWorkspace --datacard combined_{channel}_2018_{m1}_{m2}.txt --workspace combined_{channel}_2018_{m1}_{m2}.root --fitresult fitDiagnosticsTest.root:fit_s --postfit --output postfitShapes_a1a2_{m1}_{m2}.root')
