8d4171011d1838ba273192d416970af0eca462e2
skkwan <skwan@princeton.edu>
2025-07-04 20:27:19 +0200

    add helper python script

---
+++ CombineTools/bin/runCountingExperiment.py
@@ -0,0 +1,5 @@
+import os 
+
+os.system("countingExperiment-ZHMET mm 2018 700 1")
+
+os.system("combineCards.py SR1=zhmet_mm_1_2018_TChiZH_700_1.txt SR2=zhmet_mm_2_2018_TChiZH_700_1.txt > together.txt")
